40e4b92f3a6d4848e5de61d47661e2ce1740c494
Kai Nuoranen <kai.nuoranen@gmail.com>
2019-01-23 21:57:25 +0200

    Lisatty API controllerit projektiin. Entity Framework ja scaffolding db puuttuu!

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/CustomerApiController.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreBackend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/customers")]
+    [ApiController]
+    public class CustomerApiController : ControllerBase
+    {
+        [HttpGet]
+        [Route("")]
+        public List<Customers> GetAll()
+        {
+            NorthwindContext context = new NorthwindContext();
+            List<Customers> all = context.Customers.ToList();
+
+            return all;
+        }
+
+        [HttpGet]
+        [Route("{customerid}")]
+        public Customers GetSingle(string customerid)
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            if (customerid != null)
+            {
+                Customers customer = context.Customers.Find(customerid);
+                return customer;
+            }
+            return null;
+        }
+
+        [HttpPost]
+        [Route("")]
+        public Customers PostCreateNew(Customers customer)
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            context.Customers.Add(customer);
+            context.SaveChanges();
+
+            return customer;
+        }
+
+        // muokkaus
+        [HttpPut]
+        [Route("{customerid}")]
+        public Customers PutEdit([FromRoute] string customerid,
+           [FromBody] Customers newData)
+        {
+            NorthwindContext context = new NorthwindContext();
+
+            if (customerid != null)
+            {
+                Customers customer = context.Customers.Find(customerid);
+
+                if (customer != null)
+                {
+                    customer.CompanyName = newData.CompanyName;
+                    customer.ContactName = newData.ContactName;
+                    customer.City = newData.City;
+                    customer.Country = newData.Country;
+                    // ...
+
+                    context.SaveChanges();
+                }
+
+                return customer;
+            }
+            return null;
+        }
+    }
+}
---
+++ SQLtesti/SQLtesti/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLtesti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aloitetaan tietokannan kasittely.");
+            string yhteysmerkkijono = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
+            SqlConnection yhteys = new SqlConnection(yhteysmerkkijono);
+
+            yhteys.Open();
+            Console.WriteLine("Tietokantayhteys ajettu");
+
+            string sql = "SELECT * FROM Customers WHERE Country = 'Finland'";
+            SqlCommand komento = new SqlCommand(sql, yhteys);
+
+            SqlDataReader lukija = komento.ExecuteReader();
+            while (lukija.Read())
+            {
+                string asnro = lukija.GetString(0);
+                string nimi = lukija.GetString(1);
+                string maa = lukija.GetString(8);
+
+                Console.WriteLine($"{asnro}\t{nimi}\t{maa}");
+            }
+
+            Console.WriteLine("Tietokantakysely suoritettu.");
+
+            Console.ReadLine();
+
+            
+
+        }
+    }
+}
